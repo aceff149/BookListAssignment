4e13b4a51a0866b80ce7bb397bdc9b47b707b5fc
Allie Efferson <allie.efferson@gmail.com>
2025-04-27 19:36:25 -0500

    update

---
+++ index.js
@@ -0,0 +1,47 @@
+const books = [ 
+    { 
+      title: "The Great Gatsby", 
+      author: "F. Scott Fitzgerald", 
+      image: "https://ia600100.us.archive.org/view_archive.php?archive=/5/items/l_covers_0012/l_covers_0012_36.zip&file=0012364437-L.jpg"
+    }, 
+    { 
+      title: "1984", 
+      author: "George Orwell", 
+      image: "https://covers.openlibrary.org/b/id/14845126-L.jpg" 
+    }, 
+    { 
+      title: "Pride and Prejudice", 
+      author: "Jane Austen", 
+      image: "https://covers.openlibrary.org/b/id/14858485-L.jpg" 
+    }, 
+    { 
+      title: "To Kill a Mockingbird", 
+      author: "Harper Lee", 
+      image: "https://covers.openlibrary.org/b/id/14856323-M.jpg" 
+    }, 
+    { 
+      title: "Moby Dick", 
+      author: "Herman Melville", 
+      image: "https://ia800505.us.archive.org/view_archive.php?archive=/5/items/m_covers_0012/m_covers_0012_62.zip&file=0012621992-M.jpg" 
+    } 
+  ]; 
+  
+  
+  function fetchBooks () {
+    let promiseFetchBooks = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve (books)
+      }, 3000);  
+    })
+    return promiseFetchBooks
+  }
+  
+  fetchBooks()
+    .then((arrBooks)=>{
+      arrBooks.forEach(book => {
+        let newHeading = document.createElement ("h3")
+        newHeading.innerText = book["title"]
+        document.getElementById ("BookList").append (newHeading)
+      });
+    })
+    .catch ((error)=>console.log (error))
